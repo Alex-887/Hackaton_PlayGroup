7c573cd74847c0ed3c6a3b696f14fe15eb98dd66
yogan14 <dalmeidiog@hotmail.com>
2020-04-03 23:46:05 +0200

    Add register activity

--- app/src/main/java/com/example/hackathon_playgroupapp/RegisterActivity.java
+++ app/src/main/java/com/example/hackathon_playgroupapp/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.hackathon_playgroupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+
 }
